5c1325e91b591d9bddd5d5416152e8095674aaac
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-03-31 21:45:33 +0800

    Update vtFetch.php

--- php/vtFetch.php
+++ php/vtFetch.php
@@ -11,46 +11,56 @@
                 echo' <div id="F-container">';
                 echo '<a href="#" id="F-position" style="float: left;">'.$poss["position_name"].'</a><hr>';
                 // insert candidate divs here
-                    echo '<div class="candidate-box" >';
-                    echo '<div><label class="checkbox"><input type="radio" name="'.$poss["heirarchy_id"].'"  id="vote" value = "Abstain"><span class="checkmark"></span><br><br><b> Abstain </b></label></div></div>'; // display abstain choice
-                        echo '<div class="candidate-box" >';
-                        echo '<div>
-                                       <label class="checkbox">
-                                           <input type="radio" name="'.$poss["heirarchy_id"].'" id="vote">
-                                           <span class="checkmark"></span>
-                                                   <a href="#"><img src="#" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="Candidate" ></a>
-                                                   <div class="candidate-info">';
+                    echo '
+                    <div class="candidate-box" >';
+                    echo '
+                        <div>
+                            <label class="checkbox">
+                                <input type="radio" name="'.$poss["heirarchy_id"].'"  id="vote" value = "Abstain">
+                                    <span class="checkmark"></span><br><br><b> Abstain </b>
+                            </label>
+                        </div>
+                    </div>'; // display abstain choice
+                    echo '<div class="candidate-box" >
+                        <div>
+                            <label class="checkbox">
+                                <input type="radio" name="'.$poss["heirarchy_id"].'" id="vote">
+                                    <span class="checkmark"></span>
+                                    <a href="#"><img src="#" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="Candidate" ></a>
+                                        <div class="candidate-info">';
                 
                         // display candidate
-                        echo '<a href="#" id="F-CandidateName"><b> Candidate name:' . $poss["fname"]. " " . $poss["lname"]  . '</b></a><br><a href="#" id="F-Partylist"> Party: ' . $poss["party_name"]. '</a><br><a href="#" id="F-Platform"> Platform: ' . $poss["platform_info"]. '</a>';
+                        echo '<a href="#" id="F-CandidateName"><b> Candidate name:' . $poss["fname"]. " " . $poss["lname"]  . '</b></a><br><a href="#" id="F-Partylist"> Party: ' . $poss["party_name"]. '</a><br>
+                        <a href="#" id="F-Platform"> Platform: ' . $poss["platform_info"]. '</a>';
                 // end of candidate div
-                 echo'                              </div>
-                                        </label>
-                                </div>
-                     </div>';
+                            echo'</div>
+                            </label>
+                        </div>
+                    </div>';
             // end of position div
                
             
              
             }
             else{
-                        echo '<div class="candidate-box" >';
-                        echo '<div>
-                                       <label class="checkbox">
-                                           <input type="radio" name="'.$poss["heirarchy_id"].'" id="vote">
-                                           <span class="checkmark"></span>
-                                                   <a href="#"><img src="#" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="Candidate" ></a>
-                                                   <div class="candidate-info">';
-                
-                        // display candidate
-                        echo '<a href="#" id="F-CandidateName"><b> Candidate name:' . $poss["fname"] . " " . $poss["lname"]  . '</b></a><br><a href="#" id="F-Partylist"> Party: ' . $poss["party_name"]. '</a><br><a href="#" id="F-Platform"> Platform: ' . $poss["platform_info"]. '</a>';
-                // end of candidate div
-                 echo'                              </div>
-                                        </label>
-                                </div>
-                     </div>';
-    
+                echo '<div class="candidate-box" >';
+                echo '<div>
+                    <label class="checkbox">
+                        <input type="radio" name="'.$poss["heirarchy_id"].'" id="vote">
+                        <span class="checkmark"></span>
+                                <a href="#"><img src="#" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="Candidate" ></a>
+                                <div class="candidate-info">';
+        
+                // display candidate
+                echo '<a href="#" id="F-CandidateName"><b> Candidate name:' . $poss["fname"] . " " . $poss["lname"]  . '</b></a><br><a href="#" id="F-Partylist"> Party: ' . $poss["party_name"]. '</a><br><a href="#" id="F-Platform"> Platform: ' . $poss["platform_info"]. '</a>';
+        // end of candidate div
+            echo'                              </div>
+                                </label>
+                        </div>
+                </div>';
+
             }
+            
         }
         echo'    </div>';
     }
