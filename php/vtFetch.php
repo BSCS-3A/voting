9bae1242e99b0502fbca67d52bc083b68109026a
Towpi <70870949+Towpi@users.noreply.github.com>
2021-04-03 18:51:56 +0800

    Update vtFetch.php

--- php/vtFetch.php
+++ php/vtFetch.php
@@ -1,13 +1,20 @@
 <?php
     function generateBallot($positions){
+        session_start();
+        $voter_glvl = $_SESSION['grade_level'];
         $heir_id = 0;
         echo'    <div>';
+        $counter = 0;
         while($poss = $positions->fetch_assoc()){   // loop through all positions
             if($heir_id != $poss["heirarchy_id"]){
                 $heir_id = $poss["heirarchy_id"];
                 // insert position div here
+                 if(($counter % 2) != 0){
+                  echo'    </div>';
+                }
                 echo'    </div>';
                 echo' <div id="F-container">';
+                $counter = 0;
                 echo '<a href="#" id="F-position" style="float: left;">'.$poss["position_name"].'</a><hr>';
                 // insert candidate divs here
                     echo '<div class="candidate-box" >';
@@ -26,19 +33,20 @@
                  echo'                              </div>
                                         </label>
                                 </div>
-                     </div>';
+                     ';
+                     $counter++;
             // end of position div
                
             
              
             }
             else{
-                        echo '<div class="candidate-box" >';
+              if(($counter % 2) != 0){
                         echo '<div>
                                        <label class="checkbox">
                                            <input type="radio" name="'.$poss["heirarchy_id"].'" id="vote" value="'.$poss["candidate_id"].'">
                                            <span class="checkmark"></span>
-                                                   <a href="#"><img src="'.$poss["photo"].'" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="' . $poss["fname"]. " " . $poss["lname"]  . '" ></a>
+                                                    <a href="#"><img src="'.$poss["photo"].'" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="' . $poss["fname"]. " " . $poss["lname"]  . '" ></a>
                                                    <div class="candidate-info">';
                 
                         // display candidate
@@ -48,7 +56,26 @@
                                         </label>
                                 </div>
                      </div>';
-    
+                     $counter++;
+                   }
+                   else{
+                    echo '<div class="candidate-box" >';
+                    echo '<div>
+                                       <label class="checkbox">
+                                           <input type="radio" name="'.$poss["heirarchy_id"].'" id="vote" value="'.$poss["candidate_id"].'">
+                                           <span class="checkmark"></span>
+                                                    <a href="#"><img src="'.$poss["photo"].'" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="' . $poss["fname"]. " " . $poss["lname"]  . '" ></a>
+                                                   <div class="candidate-info">';
+                
+                        // display candidate
+                        echo '<a href="#" id="F-CandidateName"><b> Candidate name:' . $poss["fname"] . " " . $poss["lname"]  . '</b></a><br><a href="#" id="F-Partylist"> Party: ' . $poss["party_name"]. '</a><br><a href="#" id="F-Platform"> Platform: ' . $poss["platform_info"]. '</a>';
+                // end of candidate div
+                 echo'                              </div>
+                                        </label>
+                                </div>
+                     ';
+                     $counter++;
+                   }
             }
         }
         echo'    </div>';
