a18fea72be51eadbe8aee091c0080b98f6f7f972
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-04 20:38:21 +0800

    Update vtFetch.php

--- php/vtFetch.php
+++ php/vtFetch.php
@@ -3,10 +3,10 @@
       echo '<label class="checkbox">
                     <input type="radio" name="'.$poss["heirarchy_id"].'" id="vote" value="'.$poss["candidate_id"].'">
                     <span class="checkmark"></span>
-                    <a href="#"><img src="'.$poss["photo"].'" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="Candidate" ></a>
+                    <a href=""><img src="'.$poss["photo"].'" class="candidate-photo" style="float: left; width: 100px; height: 100px;" alt="Candidate" ></a>
                     
                   <div class="candidate-info">';
-            echo '<a href="#" id="F-CandidateName"><b> Candidate name:' .$poss["fname"]. " " . $poss["lname"]. '</b></a><br><a href="#" id="F-Partylist"> Party: ' .$poss["party_name"]. '</a><br><a href="#" id="F-Platform"> Platform: ' . $poss["platform_info"]. '</a>
+            echo '<a href="" id="F-CandidateName"><b> Name: ' .$poss["fname"]. " " . $poss["lname"]. '</b></a><br><a href="" id="F-Partylist"> Party: ' .$poss["party_name"]. '</a><br><a href="" id="F-Platform"> Platform: ' . $poss["platform_info"]. '</a>
           </div>
                </label>';
 
@@ -27,7 +27,7 @@
                 echo'    </div>';
                 echo' <div id="F-container">';
                 $counter = 0;
-                echo '<a href="#" id="F-position" style="float: left;">'.$poss["position_name"].'</a><hr>';
+                echo '<a href="" id="F-position" style="float: left;">'.$poss["position_name"].'</a><hr>';
                 echo '<div><label class="checkbox"><input type="radio" name="'.$poss["heirarchy_id"].'"  id="vote" value = "Abstain"><span class="checkmark"></span><b> Abstain </b></label></div>';
                 // insert candidate divs here
 
@@ -44,20 +44,16 @@
             else{
                 if(($counter % 2) != 0){
                         echo '<div>';
-                        // display candidate
-                          showCandidate($poss);
-                // end of candidate div
-                        echo'</div>
-                            </div>';
+                        showCandidate($poss);   // display candidate
+                          echo'</div>
+                          </div>';              // end of candidate div
                      $counter++;
                    }
                    else{
                     echo '<div class="candidate-box" >';
                         echo '<div>';
-                        // display candidate
-                          showCandidate($poss);
-                // end of candidate div
-                        echo'</div>';
+                        showCandidate($poss);   // display candidate
+                          echo'</div>';         // end of candidate div
                      $counter++;
                    }
     
