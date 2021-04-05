dfdd3fc7b08d912029b93d01b529038cd1204bc1
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-05 23:34:58 +0800

    Remove indents

--- js/modals.js
+++ js/modals.js
@@ -1,49 +1,49 @@
-      var xhttp = new XMLHttpRequest();
-      // Get the modals
-      var confirmModal = document.getElementById("confirmation");
-      var receiptModal = document.getElementById("receipt");
-      
-      // Get the buttons
-      var confirmBtn = document.getElementById("vote-btn"); 
-      var confBtn = document.getElementById("confirm-button");
-      var cancelBtn = document.getElementById("return-button");
-      
-      //////////////// confirmation/////////////////////
-      
-      // When the user clicks the submit button, open the modal 
-      confirmBtn.onclick = function() {
-        confirmModal.style.display = "block";
-      }
+var xhttp = new XMLHttpRequest();
+// Get the modals
+var confirmModal = document.getElementById("confirmation");
+var receiptModal = document.getElementById("receipt");
 
-      // When the user clicks on <span>, close the modal
-      cancelBtn.onclick = function() {
-        confirmModal.style.display = "none";
-      }
+// Get the buttons
+var confirmBtn = document.getElementById("vote-btn"); 
+var confBtn = document.getElementById("confirm-button");
+var cancelBtn = document.getElementById("return-button");
 
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == receiptModal) {
-          receiptModal.style.display = "none";
-        }
-      }
-      //////////////// receipt/////////////////////
-    
+//////////////// confirmation/////////////////////
 
-      // When the user clicks the button, open the modal 
-      confBtn.onclick = function() {
-        confirmModal.style.display = "none";
-        receiptModal.style.display = "block";
-        // < ?php submit()?>
-      }
+// When the user clicks the submit button, open the modal 
+confirmBtn.onclick = function() {
+  confirmModal.style.display = "block";
+}
 
-      // When the user clicks on <span> (x), close the modal
-      span.onclick = function() {
-        receiptModal.style.display = "none";
-      }
+// When the user clicks on <span>, close the modal
+cancelBtn.onclick = function() {
+  confirmModal.style.display = "none";
+}
 
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
-        if (event.target == receiptModal) {
-          receiptModal.style.display = "none";
-        }
-      }
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == receiptModal) {
+    receiptModal.style.display = "none";
+  }
+}
+//////////////// receipt/////////////////////
+
+
+// When the user clicks the button, open the modal 
+confBtn.onclick = function() {
+  confirmModal.style.display = "none";
+  receiptModal.style.display = "block";
+  // < ?php submit()?>
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  receiptModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == receiptModal) {
+    receiptModal.style.display = "none";
+  }
+}
