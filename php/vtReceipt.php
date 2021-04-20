dc0b4514addebeb702bd2593d55cb3775f265712
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-20 22:28:09 +0800

    Update vtReceipt.php

--- php/vtReceipt.php
+++ php/vtReceipt.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="../css/font-awesome.css">
     <link rel="stylesheet" type="text/css" href="../css/layout.css">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="../css/errors.css">
     <script src="https://code.jquery.com/jquery-3.5.0.js"></script>
     <script src="https://kit.fontawesome.com/a076d05399.js"></script>
     <title>Download receipt</title>
@@ -23,35 +24,35 @@
     <header id="F-header" style="text-align:center"><b>VOTE RECEIPT</b></header><br>
 
     <main>
-      <div id="download-receipt-page" class="F-download-receipt-page">
-      <div id="error-message-container" class="error-message-container">
-        <div id="F-receipt-message" class="F-receipt-message">
-          <!-- insert message here -->
-          <?php
-            if(isValidUser($conn)){
-              if(isValidTime()){// Not yet implemented
-                if(!isVoted($conn)){
-                  require "vtSubmit.php";
-                  echo "<h4>Your votes were submitted successfully! Here is a copy of your receipt</h4>";
-                }
-                else{ // Already Voted
-                    echo "<h4>You have already cast your votes.</h4>";
+      <div id="download-receipt-page" class="F-download-receipt-page">			
+				<div id="election-finished-msg" class="error-message">
+          <div id="F-receipt-message" class="F-receipt-message">
+            <!-- insert message here -->
+            <?php
+              if(isValidUser($conn)){
+                if(isValidTime()){// Not yet implemented
+                  if(!isVoted($conn)){
+                    require "vtSubmit.php";
+                    echo "<h3>Your votes were submitted successfully! Here is a copy of your receipt</h3>";
+                  }
+                  else{ // Already Voted
+                      echo "<h3>You have already cast your votes.</h3>";
+                  }
                 }
               }
+              else{ // Invalid user; destroy session and return to login
+                session_unset();    // remove all session variables
+                session_destroy();  // destroy session
+                header("Location: ../index.php");
             }
-            else{ // Invalid user; destroy session and return to login
-              session_unset();    // remove all session variables
-              session_destroy();  // destroy session
-              header("Location: ../index.php");
-          }
-          ?>
-        </div>
-        <div id="receipt-page-buttons" class="F-receipt-page-buttons">
-          <button type="button" class="F-downloadReceiptBTN" id="dl-receipt">Download Receipt</button>
-          <button type="button" class="F-goToHomeBTN" id="gt-home">Go to Home</button>
+            ?>
+          </div>
+          <div id="receipt-page-buttons" class="F-receipt-page-buttons">
+            <button type="button" class="F-downloadReceiptBTN" id="dl-receipt">Download Receipt</button>
+            <button type="button" class="F-goToHomeBTN" id="gt-home">Go to Home</button>
+          </div>
         </div>
       </div>
-        </div>
     </main>
 
     <script>
