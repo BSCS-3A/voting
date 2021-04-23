a229bb5f255f0def2b7098ac5f00ff37c3a16014
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-23 23:57:57 +0800

    Moved html inside php

--- php/vtReceipt.php
+++ php/vtReceipt.php
@@ -21,73 +21,74 @@
 		include 'navstudent.php';
 		require "connect.php";
 		require "vtValSan.php";
-	?>
+	
+    if(isValidUser($conn)){
+      if(!isVoted($conn)){
+        $sched_row = $conn->query("SELECT * FROM `vote_event` WHERE `vote_event_id` = 1");
+        $sched = $sched_row->fetch_assoc();
 
-		<header id="F-header"  style="text-align: center;"><b>VOTE RECEIPT</b></header><br>
+        $start_time = strtotime($sched['start_date']);
+        $end_time = strtotime($sched['end_date']);
+        $access_time = time();
 
-		<main>
-			<div id="download-receipt-page" class="F-download-receipt-container">
-				<div class="F-receipt-message">
-					<?php
-              			if(isValidUser($conn)){
-                			if(!isVoted($conn)){
-                    		$sched_row = $conn->query("SELECT * FROM `vote_event` WHERE `vote_event_id` = 1");
-                    		$sched = $sched_row->fetch_assoc();
-
-                    		$start_time = strtotime($sched['start_date']);
-                    		$end_time = strtotime($sched['end_date']);
-                    		$access_time = time();
-
-                    		if(empty($sched)){
-                        	header("Location: ../html/no_election_scheduled.html");
-                        	exit();
-                    		}
-                    		else if($access_time > $end_time){
-                        	header("Location: ../html/election_finished.html");
-                        	exit();
-                    		}
-                    		else if($access_time < $start_time){
-                        	header("Location: ../html/election_not_yet_started.html");
-                        	exit();
-                    		}
-                    		else if($access_time >= $start_time && $access_time <= $end_time){
-                      		require "vtSubmit.php";
-                      		echo "<h3>Your votes were submitted successfully! Here is a copy of your vote receipt</h3>";
-                    		}
-                		}
-                		else{ // Already Voted
-                  			echo "<h3>You have already voted. Here is a copy of your vote receipt.</h3>";
-                		}
-            			}
-            			else{ // Invalid user; destroy session and return to login
-              				session_unset();    // remove all session variables
-              				session_destroy();  // destroy session
-              				header("Location: ../index.php");
-              				exit();
-            				}
-            			?>
-        			</div>
-			</div>
-				<div id="receipt-page-buttons" class="F-receipt-page-buttons">
+        if(empty($sched)){
+          header("Location: ../html/no_election_scheduled.html");
+          exit();
+        }
+        else if($access_time > $end_time){
+          header("Location: ../html/election_finished.html");
+          exit();
+        }
+        else if($access_time < $start_time){
+          header("Location: ../html/election_not_yet_started.html");
+          exit();
+        }
+        else if($access_time >= $start_time && $access_time <= $end_time){
+          require "vtSubmit.php";
+          echo '<header id="F-header"  style="text-align: center;"><b>VOTE RECEIPT</b></header><br>';
+          echo '<main>';
+          echo '<div id="download-receipt-page" class="F-download-receipt-container">';
+          echo '<div class="F-receipt-message">';
+          echo "<h3>Your votes were submitted successfully! Here is a copy of your vote receipt</h3>";
+          echo '</div></div>';
+          
+        }
+      }
+      else{ // Already Voted
+        echo '<header id="F-header"  style="text-align: center;"><b>VOTE RECEIPT</b></header><br>';
+        echo '<main>';
+        echo '<div id="download-receipt-page" class="F-download-receipt-container">';
+        echo '<div class="F-receipt-message">';
+        echo "<h3>You have already voted. Here is a copy of your vote receipt.</h3>";
+        echo '</div></div>';
+      }
+    }
+    else{ // Invalid user; destroy session and return to login
+      session_unset();    // remove all session variables
+      session_destroy();  // destroy session
+      header("Location: ../index.php");
+      exit();
+    }
+  ?>
+          <div id="receipt-page-buttons" class="F-receipt-page-buttons">
 					<button type="button" class="F-downloadReceiptBTN">Download Receipt</button>
 					<button type="button" class="F-goToHomeBTN">Go to Home</button>
 				</div>
 		</main>
+  <script>
+        // Get Download Receipt button
+        var download = document.getElementById("dl-receipt");
 
-		<script>
-      		// Get Download Receipt button
-      		var download = document.getElementById("dl-receipt");
+        // Get Home button
+        var home = document.getElementById("gt-home");
 
-      		// Get Home button
-      		var home = document.getElementById("gt-home");
+        download.onclick = function() {
+        location.href = "PDF/generatepdf.php";
+        }
 
-      		download.onclick = function() {
-        	location.href = "PDF/generatepdf.php";
-      		}
-
-      		home.onclick = function() {
-        	location.href = "/*Dashboard*/";
-      		}
-		</script>
+        home.onclick = function() {
+        location.href = "/*Dashboard*/";
+        }
+  </script>
 </body>
 </html>
