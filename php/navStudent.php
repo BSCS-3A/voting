8584e9824141e99795358d4dd8bac5171ee26418
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-04-27 10:14:45 +0800

    Update navStudent.php

--- php/navStudent.php
+++ php/navStudent.php
@@ -89,6 +89,40 @@
         $('.icon').click(function () {
             $('span').toggleClass("cancel");
         });
+
+        window.addEventListener("auxclick", (event) => {
+            if (event.button === 1) event.preventDefault();
+        });
+        document.addEventListener('contextmenu', function(e) {
+          e.preventDefault();
+        });
+
+        $(document).keydown(function(e){ 
+            if (event.keyCode == 123) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'A'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'S'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'D'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) { 
+                return false; 
+            } 
+            if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) { 
+                return false; 
+            } 
+        }); 
     </script>
 </body>
 
