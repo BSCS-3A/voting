5971b3085446f24848e268babc25592180161fb5
Qetzyl <johnpauljaynario.azore@bicol-u.edu.ph>
2021-05-01 20:05:28 +0800

    Update vtValSan.php

--- php/vtValSan.php
+++ php/vtValSan.php
@@ -36,8 +36,11 @@
         // check if user has already voted
         $stud_id = $conn->real_escape_string($_SESSION['student_id']);
         $voter = $conn->query("SELECT * FROM student WHERE student_id = $stud_id");
-        $voter = $conn->query("SELECT * FROM vote WHERE student_id = $stud_id");
-        if(empty($voter)){
+        $vote_table = $conn->query("SELECT * FROM vote WHERE student_id = $stud_id");
+        if(!empty($vote_table->fetch_assoc())){
+            return true;
+        }
+        else{
             $student = $voter->fetch_assoc();
             // see if login already has voter info
             if($student['voting_status'] == 1){
@@ -46,9 +49,6 @@
             else{
                 return false;
             }
-        }
-        else{
-            return true;
         }
     }
 
